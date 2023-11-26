894dc14e550e42523d55a6be1bfeb5449c04368a
tejasnoovosoft <tejas.noovosoft@gmail.com>
2023-11-27 01:37:04 +0530

    add .env file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const express = require('express')
 const app = express()
 
-app.listen(3000,()=>{
-    console.log("Server running at port 3000")
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
 })
