e42fe038f388c95b1c989530cbf05521e0a0e740
tejasekhande1 <ekhandetejas741@gmail.com>
2024-02-04 06:54:06 +0530

    add product api's for users

--- controllers/Product.js
+++ controllers/Product.js
@@ -109,3 +109,141 @@ exports.deleteProduct = async (req, res) => {
         });
     }
 };
+
+exports.getProductsByCategory = async (req, res) => {
+    try {
+        const category = req.params.category;
+
+        const products = await Product.find({ category: category });
+
+        return res.status(200).json({
+            success: true,
+            message: "Products Retrieved Successfully",
+            data: products
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to retrieve products by category",
+            error: error
+        });
+    }
+};
+
+exports.searchProducts = async (req, res) => {
+    try {
+
+        const searchQuery = req.query.q;
+
+        console.log("Search Query -> ", searchQuery);
+
+        if (!searchQuery) {
+            return res.status(400).json({
+                success: false,
+                message: 'Search query parameter (q) is required',
+            });
+        }
+
+        const regex = new RegExp(searchQuery, 'i');
+
+        const products = await Product.find({
+            $or: [
+                { name: { $regex: regex } },
+                { category: { $regex: regex } },
+                { description: { $regex: regex } },
+                { brand: { $regex: regex } }
+            ]
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Products Retrieved Successfully",
+            data: products
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to retrieve products by search query",
+            error: error
+        });
+    }
+};
+
+exports.searchProductsByCategory = async (req, res) => {
+    try {
+        const category = req.params.category;
+        const searchQuery = req.query.q;
+
+        if (!searchQuery) {
+            return res.status(400).json({
+                success: false,
+                message: 'Search query parameter (q) is required',
+            });
+        }
+
+        const regex = new RegExp(searchQuery, 'i');
+
+        const products = await Product.find({
+            $and: [
+                { category: category },
+                {
+                    $or: [
+                        { name: { $regex: regex } },
+                        { description: { $regex: regex } },
+                        { brand: { $regex: regex } }
+                    ]
+                }
+            ]
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Products Retrieved Successfully",
+            data: products
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to retrieve products by search query",
+            error: error
+        });
+    }
+};
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+
+        return res.status(200).json({
+            success: true,
+            message: "Products Retrieved Successfully",
+            data: products
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to retrieve products",
+            error: error
+        });
+    }
+};
+
+exports.getProductsByBrandName = async (req, res) => {
+    try {
+        const brandName = req.params.brandName;
+
+        const products = await Product.find({ brand: brandName });
+
+        return res.status(200).json({
+            success: true,
+            message: "Products Retrieved Successfully",
+            data: products
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to retrieve products by brand name",
+            error: error
+        });
+    }
+};
--- routes/Product.js
+++ routes/Product.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { auth, isAdmin, isUser } = require('../middlewares/Auth')
-const { addProduct, deleteProduct, updateProduct } = require('../controllers/Product')
+const { addProduct, deleteProduct, updateProduct, getProductsByCategory, searchProducts, getAllProducts, searchProductsByCategory, getProductsByBrandName } = require('../controllers/Product')
 
-router.post('/add', auth, isAdmin, addProduct)
-router.delete('/delete/:productId', auth, isAdmin, deleteProduct)
-router.put('/update/:productId', auth, isAdmin, updateProduct)
+router.post('/add', auth, isAdmin, addProduct);
+router.delete('/delete/:productId', auth, isAdmin, deleteProduct);
+router.put('/update/:productId', auth, isAdmin, updateProduct);
+
+router.get('/search', auth, isUser, searchProducts);
+router.get('/:category', auth, isUser, getProductsByCategory);
+router.get('/:category/search', auth, isUser, searchProductsByCategory);
+router.get('/brand/:brandName', auth, isUser, getProductsByBrandName)
+router.get('/', auth, isUser, getAllProducts);
 
 module.exports = router;
